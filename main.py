30e7974965e88d1c96fe81f431e7bd7b0ce4eac1
Qndemn <g4sharpoon@outlook.com>
2026-02-10 21:48:46 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import time
 import random
 import sys
 
-def slow_print(text, delay=0.05):
+sanity = 100
+
+def slow_print(text, delay=0.1):
     """
     Prints text character by character to simulate a typing effect.
 
@@ -15,3 +17,29 @@ def slow_print(text, delay=0.05):
         sys.stdout.flush()
         time.sleep(delay)
     print()
+
+def start():
+    global sanity
+    slow_print("...where am I?")
+    time.sleep(2)
+    slow_print("\n...")
+    time.sleep(2)
+    slow_print("\nHello? Anyone?")
+    time.sleep(1)
+    slow_print("\n...")
+    time.sleep(1)
+    slow_print("\nGet up?")
+    input("\ny/n ")
+    slow_print("of course I'm getting up.")
+    time.sleep(1)
+    slow_print("\n< !HELLO THERE! >")
+    time.sleep(2)
+    slow_print("\n...")
+    time.sleep(1)
+    slow_print("\n< !WELL? RESPOND! >")
+    slow_print("\nAsk a question?")
+    choice = input("y/n ")
+    if choice == "y":
+        slow_print()
+
+start()
