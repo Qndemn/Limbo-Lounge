5138e6449dac5b67f2690a908091199bb3f932d6
Qndemn <g4sharpoon@outlook.com>
2026-02-10 23:06:31 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 sanity = 100
 
-def slow_print(text, delay=0.1):
+def slow_print(text, delay=0.085):
     """
     Prints text character by character to simulate a typing effect.
 
@@ -40,6 +40,17 @@ def start():
     slow_print("\nAsk a question?")
     choice = input("y/n ")
     if choice == "y":
-        slow_print()
+        slow_print("What do you want to ask? \n1. Who are you? \n2. Where am I? \n3. Am I... Dead?")
+        question = input("1, 2 or 3? ")
+        if question == "1":
+            slow_print("< !I'M AVICE! SHORT FOR AVARICE! >")
+        if question == "2":
+            slow_print("< !YOU, MY FRIEND, ARE IN THE TSO! >")
+            slow_print("< !BUT WE JUST CALL IT THE LIMBO LOUNGE! >")
+        if question == "3":
+            slow_print("< !! >")
+            slow_print("< !...! >")
+            slow_print("< !UH, YES! >")
+
 
 start()
